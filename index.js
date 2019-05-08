7aeba2382708061140531eb992c24f95574dfe94
evilamaior <contatoevilamaior@gmail.com>
2019-05-08 14:07:43 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const arrayNumber = card => {
   return card.split('').map(Number);
 };
-const checkEqualDigits = card => arrayNumber(card).every((digit) => digit === arrayNumber(card)[0]) ? false : true;
+const checkEqualDigits = card => arrayNumber(card).every((digit) => digit === arrayNumber(card)[0]);
 const calculateDigit = card => arrayNumber(card)
   .map((element, i) => i % 2 === 0 ? element * 2 : element)
   .map((element) => element > 9 ? element - 9 : element)
   .reduce((accumulator, currentValue) => accumulator + currentValue);
 const cardValidator = card => {
-  return (calculateDigit(card) % 10 === 0 && checkEqualDigits(card));
+  return (checkEqualDigits(card) && card.length === 16 && calculateDigit(card) % 10 === 0);
 };
 module.exports.cardValidator = cardValidator;
